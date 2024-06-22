2dc0f1a6e32e6a8e346fe220825a35322fe3b105
Ali <50430579+aliAskariAu@users.noreply.github.com>
2024-06-22 17:10:29 +1000

    Update and rename database.php.template to database.php

--- model/database.php
+++ model/database.php
@@ -1,9 +1,9 @@
 <?php
 	//database connection details
-	$host = '${DBHost}';
-	$user = '${DBUser}';
-	$password = '${DBPassword}';
-	$database = '${DBName}';
+	$host = $HOST;
+	$user = $USER;
+	$password = $PASSWORD;
+	$database = $DATABASE;
 
 	//connect to database with a try/catch statement
 	//if the connection is not successful display the error message via database_error.php
